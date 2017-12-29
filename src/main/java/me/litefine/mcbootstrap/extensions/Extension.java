1c1f8d932df8fad9bcf1ec0147e2bb91a3bd248f
Sergey <ssb.3185@gmail.com>
2017-12-29 19:10:34 +0300

    Added extensions system.

---
+++ src/main/java/me/litefine/mcbootstrap/extensions/Extension.java
@@ -0,0 +1,40 @@
+package me.litefine.mcbootstrap.extensions;
+
+import me.litefine.mcbootstrap.objects.booting.BootingServer;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by LITEFINE IDEA on 29.12.2017.
+ */
+public class Extension {
+
+    protected final ExecutorService executorService = Executors.newSingleThreadExecutor();
+    protected String name, author, version;
+
+    public void onSystemStartup() {}
+
+    public void onSystemShutdown() {}
+
+    public void onServerStartup(BootingServer object) {}
+
+    public void onServerShutdown(BootingServer object) {}
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public ExecutorService getExecutorService() {
+        return executorService;
+    }
+
+}
---
+++ src/main/java/me/litefine/mcbootstrap/extensions/ExtensionsManager.java
@@ -0,0 +1,54 @@
+package me.litefine.mcbootstrap.extensions;
+
+import me.litefine.mcbootstrap.main.MCBootstrap;
+import me.litefine.mcbootstrap.main.Settings;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+
+/**
+ * Created by LITEFINE IDEA on 29.12.2017.
+ */
+public class ExtensionsManager {
+
+    private static final List<Extension> extensions = new ArrayList<>();
+
+    public static void loadExtensions() {
+        for (File file : Settings.getExtensionsFolder().listFiles()) {
+            if (file.isFile() && file.getName().endsWith(".jar")) {
+                try {
+                    MCBootstrap.getLogger().info("Loading extension " + file.getName() + "...");
+                    JarFile jarFile = new JarFile(file);
+                    ZipEntry zipEntry = jarFile.getEntry("extension.info");
+                    if (zipEntry != null) {
+                        Properties properties = new Properties();
+                        properties.load(jarFile.getInputStream(zipEntry));
+                        URLClassLoader loader = new URLClassLoader(new URL[]{file.toURI().toURL()});
+                        Class<? extends Extension> extensionClass = Class.forName(properties.getProperty("mainClass"), true, loader).asSubclass(Extension.class);
+                        Extension extension = extensionClass.newInstance();
+                        extension.name = properties.getProperty("name");
+                        extension.author = properties.getProperty("author");
+                        extension.version = properties.getProperty("version");
+                        extensions.add(extension);
+                        MCBootstrap.getLogger().info("Extension " + extension.name + " v. " + extension.version + " of " + extension.author + " loaded into the system!");
+                    } else MCBootstrap.getLogger().warn("Extension " + file.getName() + " doesn't contains 'extension.info' file!");
+                } catch (NullPointerException | IOException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                    MCBootstrap.getLogger().warn("Can't load extension file " + file.getName() + " - " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    public static List<Extension> getExtensions() {
+        return Collections.unmodifiableList(extensions);
+    }
+
+}
